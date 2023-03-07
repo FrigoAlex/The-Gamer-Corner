525e4373af05e5888711bfb6dbff138585a801d0
FrigoAlex <alejo012903@hotmail.com>
2023-03-06 22:34:38 -0500

    🏗️ Reestructure the App and main components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import "./assets/styles/App.css";
-
+import useNavigate from "./hooks/useNavigate";
+import Navbar from "./components/navbar";
+import GamesSection from "./pages/gamesSection";
+import GameDescription from "./pages/gameDescription";
+import Footer from "./components/footer";
+import NoPage from "./components/noPage";
 function App () {
+  const { path, navigateTo } = useNavigate();
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Navbar navigateTo={navigateTo} />
+      {path === "/" ? <GamesSection navigateTo={navigateTo} /> : null}
+      {path.match(/\/game\/\d+/) ? <GameDescription /> : null}
+      {path !== "/" && !path.match(/\/game\/\d+/) ? <NoPage /> : null}
+      <Footer />
     </div>
   );
 }
