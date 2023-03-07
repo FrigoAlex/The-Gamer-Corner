0de06658607cf5585df23372731078a9987fd05a
FrigoAlex <alejo012903@hotmail.com>
2023-03-06 22:20:34 -0500

    🔥 ✨ Navbar functionality

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,21 +1,11 @@
 import "../assets/styles/navbar.css";
 
-const Navbar = () => {
+const Navbar = ({ navigateTo }) => {
   return (
     <header className="navbar">
-      <div className="logo-container">
+      <nav className="logo-container" onClick={() => navigateTo("/")}>
         <img className="header-logo" src="/src/assets/img/Logo-Gamer.png" />
         <h1 className="header-title">Gamer Corner</h1>
-      </div>
-      <nav className="nav-section">
-        <ul className="nav-elements-ul">
-          <li className="list-style-none">
-            <a className="nav-elements-li" href="index.html">Home</a>
-          </li>
-          <li className="list-style-none">
-            <a className="nav-elements-li" id="open-modal">About us</a>
-          </li>
-        </ul>
       </nav>
     </header>
   );
